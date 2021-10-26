b030a32a912d11929ba4a9bf7977ac77a2add380
rasel <russelroy15@gmail.com>
2021-10-27 03:33:12 +0600

    WIP on understand node express mongo project

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+/* 
+one time:
+1. nodemon globally install
+2. mongodb atlas user, access
+3. Network access (ip address allow)
+
+
+
+
+*/
