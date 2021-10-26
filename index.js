f401a49c926d78471e7e901818e11274836ff57d
rasel <russelroy15@gmail.com>
2021-10-27 03:44:37 +0600

    WIP on understand node express mongo project

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rzfie.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
