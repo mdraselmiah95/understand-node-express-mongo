7a0032a1efad5b56503125fb4ffa6ec9e20dc553
mdraselmiah95 <russelroy15@gmail.com>
2022-09-13 22:39:08 +0600

    new code added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const app = express();
 const port = 5000;
 
+// Connecting to Mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rzfie.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
